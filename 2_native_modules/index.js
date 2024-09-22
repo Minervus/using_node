76b28739982f67d596cfc6e1d18f3708c06be650
Tony Nguyen <tnguyen411@gmail.com>
2024-09-21 23:13:18 -0700

    trying fs.readFile

--- 2_native_modules/index.js
+++ 2_native_modules/index.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
-    if (err) throw err;
-    console.log("The file has been saved");
-});
+// fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
+//     if (err) throw err;
+//     console.log("The file has been saved");
+// });
+
+var contents = fs.readFile("message.txt", {encoding: 'utf8'});
+
+console.log(contents); 
